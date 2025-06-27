999f2c5925920295734987a1694ae360ce813b7b
Abhishek Sharma <geekysharma31@gmail.com>
2025-06-27 10:29:52 +0530

    fixes

--- src/app/api/_utils/verifyFirebaseToken.ts
+++ src/app/api/_utils/verifyFirebaseToken.ts
@@ -4,10 +4,10 @@ import { getAuth } from 'firebase-admin/auth';
 if (!getApps().length) {
   initializeApp({
     credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
+      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    } as any),
   });
 }
 
